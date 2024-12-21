16115957fc15f06bec49a47884a19ad93df710d3
Filip <91011923+MrSheep05@users.noreply.github.com>
2024-12-21 22:09:04 +0100

    Appsync (#75)
    * userSignUp lambda
    
    * etag
    
    * possible fix

--- packages/infrastructure/modules/appsync/data.role.tf
+++ packages/infrastructure/modules/appsync/data.role.tf
@@ -15,11 +15,6 @@ data "aws_iam_policy_document" "appsync_invoke_lambda" {
   statement {
     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["appsync.amazonaws.com"]
-    }
-
     actions = ["lambda:InvokeFunction"]
 
     resources = [var.lambda_functions["create_api_key"].arn]
