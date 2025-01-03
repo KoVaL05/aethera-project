d73291c2477e4166f4caa6be0d4604d20b86d3be
Filip <91011923+MrSheep05@users.noreply.github.com>
2025-01-03 23:26:34 +0100

    Appsync (#91)
    * userSignUp lambda
    
    * etag
    
    * arn

--- packages/infrastructure/modules/appsync/data.role.tf
+++ packages/infrastructure/modules/appsync/data.role.tf
@@ -17,6 +17,6 @@ data "aws_iam_policy_document" "appsync_invoke_lambda" {
 
     actions = ["lambda:InvokeFunction"]
 
-    resources = [var.lambda_functions["create_api_key"].arn, var.lambda_functions["update_api_key"]]
+    resources = [var.lambda_functions["create_api_key"].arn, var.lambda_functions["update_api_key"].arn]
   }
 }
