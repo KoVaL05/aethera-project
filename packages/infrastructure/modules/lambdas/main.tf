dd59de809b19af6fd01b9ac8b7a68a5cf53c7549
Filip Wachowski <fifiw2005@gmail.com>
2025-02-22 19:02:36 +0100

    fix

--- packages/infrastructure/modules/lambdas/main.tf
+++ packages/infrastructure/modules/lambdas/main.tf
@@ -29,8 +29,3 @@ resource "aws_lambda_function" "lambda_functions" {
   }
 }
 
-resource "aws_lambda_event_source_mapping" "lambda_dynamodb_trigger" {
-  event_source_arn  = var.api_key_stream_arn
-  function_name     = aws_lambda_function.lambda_functions["dynamodb_stream_handler"].arn
-  starting_position = "LATEST"
-}
