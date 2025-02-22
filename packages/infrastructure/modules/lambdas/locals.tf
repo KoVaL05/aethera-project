966bf7d9cfa84b85da4b8a13bb2e5f08beae810b
Filip <91011923+MrSheep05@users.noreply.github.com>
2025-02-22 20:34:16 +0100

    Dynamodb stream (#107)
    * lambda handler
    
    * zip key
    
    * fix

--- packages/infrastructure/modules/lambdas/locals.tf
+++ packages/infrastructure/modules/lambdas/locals.tf
@@ -56,21 +56,5 @@ locals {
         apiKeyTableName    = var.api_key_table_name
       }
     },
-    "dynamodb_stream_handler" = {
-      allow_agent_execution    = false
-      allow_userpool_execution = false
-
-      permissions = {
-        link_users           = false
-        api_key_table        = "none"
-        kms_api_key          = "none"
-        api_key_stream       = true
-        call_api_key_appsync = true
-      },
-      env = {
-        kmsApiKeyAliasName = var.kms_api_key_alias
-        apiKeyTableName    = var.api_key_table_name
-      }
-    },
   }
 }
