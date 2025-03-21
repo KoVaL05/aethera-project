f0a69ed4ce34add8d38a599a970cefb9cb9fd701
Filip Wachowski <fifiw2005@gmail.com>
2025-03-21 19:57:39 +0100

    fix3

--- packages/infrastructure/modules/vpc/main.endpoint.tf
+++ packages/infrastructure/modules/vpc/main.endpoint.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "appsync" {
   vpc_id            = aws_vpc.main.id
-  service_name      = format("com.amazonaws.%s.appsync-api",data.aws_region.current)
+  service_name      = format("com.amazonaws.%s.appsync-api",data.aws_region.current.name)
   vpc_endpoint_type = "Interface"
   
   subnet_ids          = [aws_subnet.private_appsync_subnet.id]
