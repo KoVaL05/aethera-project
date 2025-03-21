dd44c6be5fa583ec460bc52a78cb20f3b2d8af66
Filip Wachowski <fifiw2005@gmail.com>
2025-03-21 20:05:17 +0100

    subnet

--- packages/infrastructure/modules/vpc/main.endpoint.tf
+++ packages/infrastructure/modules/vpc/main.endpoint.tf
@@ -3,7 +3,7 @@ resource "aws_vpc_endpoint" "appsync" {
   service_name      = format("com.amazonaws.%s.appsync-api",data.aws_region.current.name)
   vpc_endpoint_type = "Interface"
   
-  subnet_ids          = [aws_subnet.private_appsync_subnet.id]
+  subnet_ids          = [aws_subnet.private_appsync_subnet_a.id,aws_subnet.private_appsync_subnet_b.id,aws_subnet.private_appsync_subnet_c.id]
   security_group_ids  = [aws_security_group.appsync_endpoint.id]
   
   private_dns_enabled = true
