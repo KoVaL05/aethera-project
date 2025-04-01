a5ca3c797bc5174f6650917fa6f08c98e5650544
Filip <91011923+MrSheep05@users.noreply.github.com>
2025-04-01 22:01:34 +0200

    fix (#115)
    * fix
    
    * appsync id

--- packages/infrastructure/modules/event_bridge/main.schedule.tf
+++ packages/infrastructure/modules/event_bridge/main.schedule.tf
@@ -2,7 +2,7 @@ resource "aws_scheduler_schedule" "schedules" {
   for_each = local.scheduler_configuration
 
   name       = format("%s-schedule-%s", var.random_name, each.key)
-  group_name = each.value.group_name
+  group_name = "default"
 
   schedule_expression = each.value.schedule
 
