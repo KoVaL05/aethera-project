8446000219d3318830b760380a8151e742191c24
Filip Wachowski <fifiw2005@gmail.com>
2025-04-01 22:17:02 +0200

    data source fix

--- packages/infrastructure/modules/appsync/data.role.tf
+++ packages/infrastructure/modules/appsync/data.role.tf
@@ -11,12 +11,22 @@ data "aws_iam_policy_document" "appsync_assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "appsync_invoke_lambda" {
+data "aws_iam_policy_document" "appsync_public_invoke_lambda" {
   statement {
     effect = "Allow"
 
     actions = ["lambda:InvokeFunction"]
 
     resources = [var.lambda_functions["create_api_key"].arn, var.lambda_functions["update_api_key"].arn]
+  }
+}
+
+data "aws_iam_policy_document" "appsync_public_invoke_lambda" {
+  statement {
+    effect = "Allow"
+
+    actions = ["lambda:InvokeFunction"]
+
+    resources = [var.lambda_functions["create_api_key_private"].arn, var.lambda_functions["update_api_key_private"].arn]
   }
 }
--- packages/infrastructure/modules/appsync/main.role.tf
+++ packages/infrastructure/modules/appsync/main.role.tf
@@ -3,19 +3,35 @@ resource "aws_iam_role" "appsync_api_key_role" {
   assume_role_policy = data.aws_iam_policy_document.appsync_assume_role.json
 }
 
-resource "aws_iam_role" "appsync_lambda_role" {
-  name               = "appsync_lambbda_role"
+resource "aws_iam_role" "appsync_public_lambda_role" {
+  name               = "appsync_public_lambbda_role"
+  assume_role_policy = data.aws_iam_policy_document.appsync_assume_role.json
+}
+
+resource "aws_iam_role" "appsync_private_lambda_role" {
+  name               = "appsync_private_lambbda_role"
   assume_role_policy = data.aws_iam_policy_document.appsync_assume_role.json
 }
 
 
-resource "aws_iam_policy" "appsync_lambda_policy" {
-  name   = format("appsync_lambda_invoke-%s", var.random_name)
+resource "aws_iam_policy" "appsync_public_lambda_policy" {
+  name   = format("appsync_public_lambda_invoke-%s", var.random_name)
+  policy = data.aws_iam_policy_document.appsync_public_invoke_lambda.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "appsync_public_lambda_invoke" {
+  role       = aws_iam_role.appsync_public_lambda_role.name
+  policy_arn = aws_iam_policy.appsync_public_lambda_policy.arn
+}
+
+resource "aws_iam_policy" "appsync_private_lambda_policy" {
+  name   = format("appsync_private_lambda_invoke-%s", var.random_name)
   policy = data.aws_iam_policy_document.appsync_invoke_lambda.json
 }
 
 
-resource "aws_iam_role_policy_attachment" "appsync_lambda_invoke" {
-  role       = aws_iam_role.appsync_lambda_role.name
-  policy_arn = aws_iam_policy.appsync_lambda_policy.arn
+resource "aws_iam_role_policy_attachment" "appsync_private_lambda_invoke" {
+  role       = aws_iam_role.appsync_private_lambda_role.name
+  policy_arn = aws_iam_policy.appsync_private_lambda_policy.arn
 }
