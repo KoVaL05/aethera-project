ae4ffdc2968f5e7e914fec9cba5855bf5ae613de
Filip Wachowski <fifiw2005@gmail.com>
2025-04-21 00:07:10 +0200

    without count

--- packages/infrastructure/modules/lambdas/main.tf
+++ packages/infrastructure/modules/lambdas/main.tf
@@ -37,7 +37,6 @@ resource "aws_lambda_function" "lambda_functions" {
 
 resource "aws_lambda_function" "lambda_output_functions" {
   for_each = local.lambda_outputs_functions_data
-  count = var.api_key_public_appsync_uri != "" ? 1 : 0
 
   function_name = format("%s_%s", each.key, var.random_name)
   role          = aws_iam_role.lambda_output_roles[each.key].arn
