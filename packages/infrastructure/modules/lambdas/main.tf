72f43b1a32f51f3abbf08c0fa5ed95951a567b3b
Filip Wachowski <fifiw2005@gmail.com>
2025-04-21 00:05:04 +0200

    separate resources

--- packages/infrastructure/modules/lambdas/main.tf
+++ packages/infrastructure/modules/lambdas/main.tf
@@ -1,5 +1,11 @@
 resource "aws_iam_role" "lambda_roles" {
-  for_each = concat(local.lambda_functions_data,local.lambda_outputs_functions_data)
+  for_each = local.lambda_functions_data
+
+  name               = format("%s-lambda-role-%s", each.key, var.random_name)
+  assume_role_policy = data.aws_iam_policy_document.basic_lambda_role.json
+}
+resource "aws_iam_role" "lambda_output_roles" {
+  for_each = local.lambda_outputs_functions_data
 
   name               = format("%s-lambda-role-%s", each.key, var.random_name)
   assume_role_policy = data.aws_iam_policy_document.basic_lambda_role.json
@@ -31,9 +37,10 @@ resource "aws_lambda_function" "lambda_functions" {
 
 resource "aws_lambda_function" "lambda_output_functions" {
   for_each = local.lambda_outputs_functions_data
+  count = var.api_key_public_appsync_uri != "" ? 1 : 0
 
   function_name = format("%s_%s", each.key, var.random_name)
-  role          = aws_iam_role.lambda_roles[each.key].arn
+  role          = aws_iam_role.lambda_output_roles[each.key].arn
   runtime       = "python3.13"
   timeout       = 60
   memory_size   = 128
