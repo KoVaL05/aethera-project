8ff12ad9f6fe4dfd7188ecd9e66c1c15852cd3cc
Filip Wachowski <fifiw2005@gmail.com>
2025-04-21 00:09:58 +0200

    s3 object

--- packages/infrastructure/modules/lambdas/data.lambdas.tf
+++ packages/infrastructure/modules/lambdas/data.lambdas.tf
@@ -4,3 +4,10 @@ data "aws_s3_object" "object" {
   bucket = var.lambdas_bucket_name
   key    = format("%s/%s.zip", each.key, each.key)
 }
+
+data "aws_s3_object" "lambdas_output_object" {
+  for_each = local.lambda_outputs_functions_data
+
+  bucket = var.lambdas_bucket_name
+  key    = format("%s/%s.zip", each.key, each.key)
+}
